591ba906156c451721cd3d6493bb26e313010ee3
kirimin <cc@kirimin.me>
2016-06-28 00:21:02 +0900

    OkHttpClientを使い回すように修正

--- app/src/main/java/me/kirimin/annictroid/_common/networks/RetrofitClient.kt
+++ app/src/main/java/me/kirimin/annictroid/_common/networks/RetrofitClient.kt
@@ -1,8 +1,6 @@
 package me.kirimin.annictroid._common.networks
 
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,12 +8,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
 
     val endPoint = "https://api.annict.com/"
+    val defaultClient = OkHttpClient()
 
     fun default(): Retrofit.Builder {
-        val client = OkHttpClient.Builder().build();
         return Retrofit.Builder()
                 .baseUrl(endPoint)
-                .client(client)
+                .client(defaultClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
     }
