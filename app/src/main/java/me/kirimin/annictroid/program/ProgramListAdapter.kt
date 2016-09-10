06473ca6e0bae287c9b0a3a6d914b03eab5939c9
kirimin <cc@kirimin.me>
2016-09-10 21:35:20 +0900

    不要なimport文を削除

--- app/src/main/java/me/kirimin/annictroid/program/ProgramListAdapter.kt
+++ app/src/main/java/me/kirimin/annictroid/program/ProgramListAdapter.kt
@@ -1,30 +1,16 @@
 package me.kirimin.annictroid.program
 
 import android.content.Context
-import android.content.Intent
 import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.*
 import me.kirimin.annictroid.R
 import me.kirimin.annictroid._common.extensions.toIntegerString
-import me.kirimin.annictroid._common.networks.RetrofitClient
-import me.kirimin.annictroid._common.networks.apis.AnnictService
 import me.kirimin.annictroid._common.networks.entities.Program
-import me.kirimin.annictroid._common.networks.entities.Record
-import me.kirimin.annictroid._common.preferences.AppPreferences
 import me.kirimin.annictroid._common.utils.ApiDateFormatter
-import me.kirimin.annictroid.episode.EpisodeActivity
-import me.kirimin.annictroid.works.WorkListAdapter
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
 
 class ProgramListAdapter(context: Context,
                          private val onItemClick: (program: Program) -> Unit,
